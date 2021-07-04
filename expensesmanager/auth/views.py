24c1c1cfa3008f8a2eca0a60afc6cafb9c273f9d
ajo01 <amyjojjm2001@gmail.com>
2021-07-04 16:36:19 -0700

    emailvalidationview & validate-email path & correct res in postman

--- expensesmanager/auth/views.py
+++ expensesmanager/auth/views.py
@@ -3,6 +3,7 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
 
 # Create your views here.
 
@@ -17,6 +18,18 @@ class UsernameValidationView(View):
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'The username already in use. Choose another username'}, status=409)
         return JsonResponse({'username_valid': True})
+
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'The email already in use'}, status=409)
+        return JsonResponse({'email_valid': True})
 
 
 class RegistrationView(View):
