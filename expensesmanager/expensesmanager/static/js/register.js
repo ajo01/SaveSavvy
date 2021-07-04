3aee26cdb85377dcaa7747074e19dda6f948cc5b
ajo01 <amyjojjm2001@gmail.com>
2021-07-04 16:27:19 -0700

    username error to valid username -> input removes is-invalid class

--- expensesmanager/expensesmanager/static/js/register.js
+++ expensesmanager/expensesmanager/static/js/register.js
@@ -2,10 +2,13 @@ console.log("register connected!");
 
 const usernameField = document.querySelector("#usernameField");
 const usernameFeedback = document.querySelector(".invalid-feedback");
+
 if (usernameField) {
   usernameField.addEventListener("keyup", (e) => {
     const usernameVal = e.target.value;
-    // console.log(usernameVal);
+    usernameField.classList.remove("is-invalid");
+    usernameFeedback.style.display = "none";
+
     if (usernameVal.length > 0) {
       fetch("/auth/validate-username", {
         body: JSON.stringify({ username: usernameVal }),
