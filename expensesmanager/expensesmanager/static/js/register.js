55213462192d14448dbbe8e37d26dce013b869e6
ajo01 <amyjojjm2001@gmail.com>
2021-07-04 16:03:22 -0700

    fetch validate-username works

--- expensesmanager/expensesmanager/static/js/register.js
+++ expensesmanager/expensesmanager/static/js/register.js
@@ -1,8 +1,19 @@
-console.log("register working!");
+console.log("register connected!");
 
 const usernameField = document.querySelector("#usernameField");
 if (usernameField) {
-  usernameField.addEventListener("keyup", () => {
-    console.log("keyup! 323");
+  usernameField.addEventListener("keyup", (e) => {
+    const usernameVal = e.target.value;
+    // console.log(usernameVal);
+    if (usernameVal.length > 0) {
+      fetch("/auth/validate-username", {
+        body: JSON.stringify({ username: usernameVal }),
+        method: "POST",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
   });
 }
