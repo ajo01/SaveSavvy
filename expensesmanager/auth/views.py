3d3e13f5ccf6f8ec401a71359457da233c5d6f37
ajo01 <amyjojjm2001@gmail.com>
2021-07-04 14:09:32 -0700

    username validation response crsf error solved

--- expensesmanager/auth/views.py
+++ expensesmanager/auth/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data('username')
+
+        if not str(username).isalnum():
+            return JsonResponse('username_error:username should only contain alphanumberic characters')
+        return JsonResponse({'username_valid': True})
 
 
 class RegistrationView(View):
