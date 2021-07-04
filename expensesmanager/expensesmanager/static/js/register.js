6c383fe9bf498e9e221ce972389d0af2b6ff8caf
ajo01 <amyjojjm2001@gmail.com>
2021-07-04 16:52:49 -0700

    email feedback works correctly

--- expensesmanager/expensesmanager/static/js/register.js
+++ expensesmanager/expensesmanager/static/js/register.js
@@ -2,6 +2,8 @@ console.log("register connected!");
 
 const usernameField = document.querySelector("#usernameField");
 const usernameFeedback = document.querySelector(".invalid-feedback");
+const emailField = document.querySelector("#emailField");
+const emailFeedback = document.querySelector(".email-feedback");
 
 if (usernameField) {
   usernameField.addEventListener("keyup", (e) => {
@@ -21,6 +23,30 @@ if (usernameField) {
             usernameField.classList.add("is-invalid");
             usernameFeedback.style.display = "block";
             usernameFeedback.innerHTML = `<p>${data.username_error}</p>`;
+          }
+        });
+    }
+  });
+}
+
+if (emailField) {
+  emailField.addEventListener("keyup", (e) => {
+    const emailVal = e.target.value;
+    emailField.classList.remove("is-invalid");
+    emailFeedback.style.display = "none";
+
+    if (emailVal.length > 0) {
+      fetch("/auth/validate-email", {
+        body: JSON.stringify({ email: emailVal }),
+        method: "POST",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.email_error) {
+            emailField.classList.add("is-invalid");
+            emailFeedback.style.display = "block";
+            emailFeedback.innerHTML = `<p>${data.email_error}</p>`;
           }
         });
     }
