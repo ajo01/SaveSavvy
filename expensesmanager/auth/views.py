078538148aeea8a0b521c55d61f787e0594dd8eb
ajo01 <amyjojjm2001@gmail.com>
2021-07-04 14:54:17 -0700

    aut views handle username in use & register.html connected to register.js

--- expensesmanager/auth/views.py
+++ expensesmanager/auth/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 import json
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,7 +13,9 @@ class UsernameValidationView(View):
         username = data['username']
 
         if not str(username).isalnum():
-            return JsonResponse({'username_error': 'username should only contain alphanumberic characters'})
+            return JsonResponse({'username_error': 'username should only contain alphanumberic characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'The username already in use. Choose another username'}, status=409)
         return JsonResponse({'username_valid': True})
 
 
