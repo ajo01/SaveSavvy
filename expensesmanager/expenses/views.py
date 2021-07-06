b708d2d6aa3329bf7b0562fd5191856259ccf3f0
ajo01 <amyjojjm2001@gmail.com>
2021-07-05 21:09:34 -0700

    going back requires login

--- expensesmanager/expenses/views.py
+++ expensesmanager/expenses/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import login
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(login_url='/auth/login')
 def index(request):
     return render(request, 'expenses/index.html')
 
