0bb2cf82813cc542f1c39ee7b6cf26062f64ebb4
ajo01 <amyjojjm2001@gmail.com>
2021-07-05 23:03:07 -0700

    debugging w pdb & loading json file

--- expensesmanager/userpreferences/views.py
+++ expensesmanager/userpreferences/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+import os
+import json
+from django.conf import settings
 # Create your views here.
 
 
 def index(request):
+    currency_data = []
+    file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+        import pdb
+        pdb.set_trace()
+
     return render(request, 'preferences/index.html')
