e908821f2ae690265b6a6833f6396acb95b09388
ajo01 <amyjojjm2001@gmail.com>
2021-07-05 23:39:19 -0700

    saving user preferences

--- expensesmanager/userpreferences/views.py
+++ expensesmanager/userpreferences/views.py
@@ -1,16 +1,63 @@
+from django.contrib.auth.models import User
+from django.core.checks import messages
 from django.shortcuts import render
 import os
 import json
 from django.conf import settings
+from . models import UserPreference
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
+    # exists = UserPreference.objects.filter(user=request.user).exists()
+    # user_preference = None
+
+    # if exists:
+    #     user_preference = UserPreference.objects.get(user=request.user)
+
+    # if request.methods == 'GET':
+    #     currency_data = []
+    #     file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    #     with open(file_path, 'r') as json_file:
+    #         data = json.load(json_file)
+    #         for key, value in data.items():
+    #             currency_data.append({'name': key, 'value': value})
+
+    #     return render(request, 'preferences/index.html', {'currencies': currency_data})
+
+    # # POST req
+    # else:
+    #     if exists:
+    #         currency = request.POST('currency')
+    #         user_preference.currency = currency
+    #         user_preference.save()
+    #     UserPreference.objects.create(user=request.user, currency=currency)
+    #     messages.success(request, 'Changes saved')
+
     currency_data = []
     file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
-        for key, value in data.items():
-            currency_data.append({'name': key, 'value': value})
+        for k, v in data.items():
+            currency_data.append({'name': k, 'value': v})
 
-    return render(request, 'preferences/index.html', {'currencies': currency_data})
+    exists = UserPreference.objects.filter(user=request.user).exists()
+    user_preferences = None
+    if exists:
+        user_preferences = UserPreference.objects.get(user=request.user)
+    if request.method == 'GET':
+
+        return render(request, 'preferences/index.html', {'currencies': currency_data,
+                                                          'user_preferences': user_preferences})
+    else:
+
+        currency = request.POST['currency']
+        if exists:
+            user_preferences.currency = currency
+            user_preferences.save()
+        else:
+            UserPreference.objects.create(user=request.user, currency=currency)
+        messages.success(request, 'Changes saved')
+        return render(request, 'preferences/index.html', {'currencies': currency_data, 'user_preferences': user_preferences})
