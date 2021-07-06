6e23c61828b9ebc8fe54374034ef8ad5a6e92cd7
ajo01 <amyjojjm2001@gmail.com>
2021-07-05 20:18:22 -0700

    activation login page works

--- expensesmanager/auth/views.py
+++ expensesmanager/auth/views.py
@@ -66,8 +66,7 @@ class RegistrationView(View):
                 user.is_active = False
                 user.save()
 
-                uid64 = force_bytes(
-                    urlsafe_base64_encode(force_bytes(user.pk)))
+                uid64 = urlsafe_base64_encode(force_bytes(user.pk))
                 domain = get_current_site(request).domain
                 link = reverse('activate', kwargs={
                                'uid64': uid64, 'token': token_generator.make_token(user)})
@@ -100,23 +99,37 @@ def send_email(username, email, url):
 
 class VerificationView(View):
     def get(self, request, uid64, token):
-        try:
-            id = force_text(urlsafe_base64_decode(uid64))
-            user = User.objects.get(pk=id)
+        id = force_text(urlsafe_base64_decode(uid64))
+        user = User.objects.get(pk=id)
 
-            if not token_generator.check_token(user, token):
-                return redirect('login'+'?message='+'User already activated')
-            if user.is_active:
-                return redirect('login')
-            user.is_active = True
-            user.save()
-
-            messages.success(request, 'Account activated!')
+        if not token_generator.check_token(user, token):
+            return redirect('login'+'?message='+'User already activated')
+        if user.is_active:
             return redirect('login')
-        except Exception as ex:
-            print('something went wrong....')
-            pass
+        user.is_active = True
+        user.save()
+
+        messages.success(request, 'Account activated!')
+        print('account activated!')
         return redirect('login')
+        # try:
+        #     id = urlsafe_base64_decode(uid64).decode()
+        #     user = User.objects.get(pk=id)
+
+        #     if not token_generator.check_token(user, token):
+        #         return redirect('login'+'?message='+'User already activated')
+        #     if user.is_active:
+        #         return redirect('login')
+        #     user.is_active = True
+        #     user.save()
+
+        #     messages.success(request, 'Account activated!')
+        #     print('account activated!')
+        #     return redirect('login')
+        # except Exception as ex:
+        #     print('something went wrong....')
+        #     pass
+        # return redirect('login')
 
 
 class LoginView(View):
