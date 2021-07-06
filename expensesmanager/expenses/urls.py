a91a25999424d1d7b178092d6cca4ce4d78cd496
ajo01 <amyjojjm2001@gmail.com>
2021-07-05 22:04:31 -0700

    updated sidebar.html and set default url for userpreferences

--- expensesmanager/expenses/urls.py
+++ expensesmanager/expenses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
     path('', views.index, name="expenses"),
-    path('add-expense', views.add_expense, name="add-expenses")
+    path('add-expense', views.add_expense, name="add-expenses"),
+    path('preferences/', include('userpreferences.urls '))
 ]
