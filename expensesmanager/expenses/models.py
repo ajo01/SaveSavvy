47f990b2ea0d1ac3268d336bdac4da8618df5169
ajo01 <amyjojjm2001@gmail.com>
2021-07-06 17:35:42 -0700

    category data structuring

--- expensesmanager/expenses/models.py
+++ expensesmanager/expenses/models.py
@@ -25,3 +25,9 @@ class Expense(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
