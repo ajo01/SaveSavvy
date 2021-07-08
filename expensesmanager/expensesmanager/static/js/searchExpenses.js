c4c45aa2ab895669f82c3784a888706326913576
ajo01 <amyjojjm2001@gmail.com>
2021-07-08 14:03:20 -0700

    searchExpenses js connected and event listener working

---
+++ expensesmanager/expensesmanager/static/js/searchExpenses.js
@@ -0,0 +1,8 @@
+console.log("searchExpenses connected");
+
+const searchField = document.querySelector("#searchField");
+
+searchField.addEventListener("keyup", (e) => {
+  const searchValue = e.target.value;
+  if (searchValue.length > 0) console.log(searchValue);
+});
