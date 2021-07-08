6c70de55de04c155cc846360f7a6f1f5ed4e26e6
ajo01 <amyjojjm2001@gmail.com>
2021-07-08 13:57:36 -0700

    search def % searchbar frontned

--- expensesmanager/expenses/views.py
+++ expensesmanager/expenses/views.py
@@ -1,18 +1,31 @@
 from django.contrib.auth import login
-from django.core import paginator
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
 from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
 
 
 @login_required(login_url='/auth/login')
 def index(request):
     expenses = Expense.objects.filter(owner=request.user)
-    paginator = Paginator(expenses, 2)
+    paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
     context = {
