aa7197f48276d3c1509d30562950626afef420c7
ajo01 <amyjojjm2001@gmail.com>
2021-07-08 17:50:49 -0700

    income app & income models

--- expensesmanager/income/models.py
+++ expensesmanager/income/models.py
@@ -5,29 +5,21 @@ from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import User
 
-# Create your models here.
 
-
-class Expense(models.Model):
+class Income(models.Model):
     amount = models.FloatField()
     date = models.DateField(default=now)
     description = models.TextField()
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    category = models.CharField(max_length=255)
+    source = models.CharField(max_length=255)
 
     # show category in queryset
     def __str__(self):
         return self.category
 
 
-Expense.objects.order_by('date')
-
-
-class Category(models.Model):
+class Source(models.Model):
     name = models.CharField(max_length=255)
-
-    class Meta:
-        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
