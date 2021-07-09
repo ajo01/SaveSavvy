9789a60e1af3a9adfefd8585b21ebefac349f158
ajo01 <amyjojjm2001@gmail.com>
2021-07-08 18:15:30 -0700

    sidebar links to income & add income page shows

--- expensesmanager/income/urls.py
+++ expensesmanager/income/urls.py
@@ -6,6 +6,9 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('', views.index, name="income"),
     path('add-income', views.add_income, name="add-income"),
-    path('preferences/', include('userpreferences.urls')),
-    path('edit-income/<int:id>', views.income_edit, name="edit-income"),
-    path('delete-income/<int:id>', views.income_delete, name="delete-income"), ]
+    # path('preferences/', include('userpreferences.urls')),
+    # path('edit-income/<int:id>', views.income_edit, name="edit-income"),
+    # path('delete-income/<int:id>', views.income_delete, name="delete-income"),
+    # path('search-income', csrf_exempt(views.search_income),
+    #      name="search-income")
+]
