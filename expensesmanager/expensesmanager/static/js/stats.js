7a4743be82a5f050aca66bd86d385b6c68db0882
ajo01 <amyjojjm2001@gmail.com>
2021-07-10 18:17:11 -0700

    doughnut charts solid colors

--- expensesmanager/expensesmanager/static/js/stats.js
+++ expensesmanager/expensesmanager/static/js/stats.js
@@ -9,22 +9,13 @@ const renderChart = (data, labels) => {
           label: "Expenses from the past year",
           data: data,
           backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
+            "rgb(75, 192, 192)", //teal
+            "rgb(255, 99, 132)", //pink
+            "rgb(54, 162, 235)", //blue
+            "rgb(255, 206, 86)", //yellow
+            "rgb(255, 159, 64)", //orange
+            "rgb(153, 102, 255)", //purple
           ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
         },
       ],
     },
