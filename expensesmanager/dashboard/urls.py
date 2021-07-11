d2f5ef68aae8c8c301ca0343f409e13ea59b16ed
ajo01 <amyjojjm2001@gmail.com>
2021-07-11 00:42:36 -0700

    dashboard page shows

--- expensesmanager/dashboard/urls.py
+++ expensesmanager/dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="dashboard"),
+    # path('stats', views.stats, name="dashboard-stats"),
 ]
--- expensesmanager/dashboard/views.py
+++ expensesmanager/dashboard/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.paginator import Paginator
+from expenses.models import Category, Expense
+from userpreferences.models import UserPreference
 
 # Create your views here.
 
 
 @login_required(login_url='/auth/login')
 def index(request):
-    return render(request, 'dashboard/index.html')
+    expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 10)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
+    context = {
+        'expenses': expenses,
+        'page_obj': page_obj,
+        'currency': currency
+    }
+    return render(request, 'dashboard/index.html', context)
